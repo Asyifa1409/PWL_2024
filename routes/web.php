a1fa8b05a9115d84eb2ed99a198a1fcd4b45e90b
Asyifa1409 <asyifa1409@gmail.com>
2024-02-25 14:32:51 +0700

    praktikum 2 bagian controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,13 +28,11 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat datang';
-});
+Route::get('/', [pageController::class, 'index']);
 
-Route::get('/about', function () {
-    return 'Asyifa Nurfadilah/2241720257';
-});
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('articles/{halo}', [PageController::class, 'about']);
 
 Route::get('/user/{Asyifa}', function ($name) {
     return 'Nama saya '.$name;
@@ -47,6 +47,17 @@ Route::get('/user/{name?}', function ($name='Asyifa') {
     return 'Nama saya '.$name;
     });
 
+ Route::get('/hello', [WelcomeController::class,'hello']);    
+
+ use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+    ]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+    ]);
     
 
     
