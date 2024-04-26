44b47dd739103d3c4ac26c2ade134d9b4a00dc1f
Asyifa1409 <asyifa1409@gmail.com>
2024-04-26 08:48:03 +0700

    jobsheet 6

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,17 +6,14 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello() {
-        return 'Hello World';
-        }
-    
-    //public function greeting(){
-    //    return view('blog.hello', ['name' => 'Andi']);
-    //}    
+    public function index() 
+    {
+        $breadcrumb = (object) [
+            'title' => 'Selamat Datang',
+            'list' => ['Home', 'Welcome']
+        ];
+        $activeMenu = 'dashboard';
+        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
 
-    public function greeting(){
-        return view('blog.hello')
-        ->with('name','Andi')
-        ->with('occupation','Astronaut');
-        }
-}
+    }
+}  
