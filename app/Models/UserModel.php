fabcd84769613dae633c9d19074663c97b42a475
Asyifa1409 <asyifa1409@gmail.com>
2024-04-26 09:03:34 +0700

    Reapply "jobsheet 6"
    This reverts commit 0ba5f7e26d5583ea468e6b749c747a2ab2bc3f76.

---
+++ app/Models/UserModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserModel extends Model
+{
+    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
+    public $timestamps = false;
+    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang digunakan
+
+    
+    protected $fillable = [
+        'user_id',
+        'level_id', 
+        'username', 
+        'nama', 
+        'password'
+    ];
+
+    public function level(): BelongsTo{
+        return $this->belongsTo(LevelModel::class,'level_id','level_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\StokController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,60 +20,75 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+}) ;
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
-
-Route::get('/hello', function () {
-    return 'Hello World';
+Route::group(['prefix' => 'level'], function(){
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}', [LevelController::class, 'show']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
 });
 
-Route::get('/world', function () {
-    return 'World';
+Route::group(['prefix' => 'barang'], function(){
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
 });
 
-Route::get('/', [pageController::class, 'index']);
+Route::group(['prefix' => 'kategori'], function(){
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
 
-Route::get('/about', [PageController::class, 'about']);
+Route::group(['prefix' => 'barang'], function(){
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
 
-Route::get('articles/{halo}', [PageController::class, 'about']);
+Route::group(['prefix' => 'stok'], function(){
+    Route::get('/', [StokController::class, 'index']);
+    Route::post('/list', [StokController::class, 'list']);
+    Route::get('/create', [StokController::class, 'create']);
+    Route::post('/', [StokController::class, 'store']);
+    Route::get('/{id}', [StokController::class, 'show']);
+    Route::get('/{id}/edit', [StokController::class, 'edit']);
+    Route::put('/{id}', [StokController::class, 'update']);
+    Route::delete('/{id}', [StokController::class, 'destroy']);
+});
 
-Route::get('/user/{Asyifa}', function ($name) {
-    return 'Nama saya '.$name;
-    });
-
-Route::get('/posts/{halo}/comments/{world}', function
-    ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});    
-
-Route::get('/user/{name?}', function ($name='Asyifa') {
-    return 'Nama saya '.$name;
-    });
-
- Route::get('/hello', [WelcomeController::class,'hello']);    
-
- use App\Http\Controllers\PhotoController;
-Route::resource('photos', PhotoController::class);
-
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-    ]);
-    Route::resource('photos', PhotoController::class)->except([
-    'create', 'store', 'update', 'destroy'
-    ]);
-
-    //Route::get('/greeting', function () {
-    //    return view('hello', ['name' => 'Andi']);
-     //   });
-
-Route::get('/greeting', [WelcomeController::class,
-'greeting']);
-
-//Route::get('/greeting', function () {
- //   return view('blog.hello', ['name' => 'Andi']);
- //   });
-    
-
-    
